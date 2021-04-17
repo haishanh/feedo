8ffa1ffbff1448928428289b8d59047a123045d6
Haishan <haishanhan@gmail.com>
2021-04-17 18:31:28 +0800

    Use fontsource to self host font

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@styles/app.css";
+import "@fontsource/sriracha/latin-400.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,11 +12,6 @@ class MyDocument extends Document {
             sizes="96x96"
             href="/favicon-96x96.png"
           />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
