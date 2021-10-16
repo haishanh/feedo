4ebdd4f9d1c48a618d74e7b2f40007fc4be9991c
Haishan <haishanhan@gmail.com>
2021-10-16 22:37:43 +0800

    Add theme-color meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#f7f7f7" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#111111" media="(prefers-color-scheme: dark)" />
       </Head>
       <Component {...pageProps} />
     </>
